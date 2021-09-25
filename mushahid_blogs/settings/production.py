c897173f9492ebb03b2af60b04a335be85b88b15
mushahid-raza <mushahid.raza@delhivery.com>
2021-09-25 16:23:03 +0530

    added-deployment-settings

--- mushahid_blogs/settings/production.py
+++ mushahid_blogs/settings/production.py
@@ -11,21 +11,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kifj)grm@6s6v6=yt@43rqeswi!3^uj=v3(^4nes49l-1vd42h'
+SECRET_KEY = getenv("SECRET_KEY")
+#'django-insecure-kifj)grm@6s6v6=yt@43rqeswi!3^uj=v3(^4nes49l-1vd42h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOSTS")
+]
 
 
 # Application definition
@@ -125,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # To look up static global dir
 STATICFILES_DIRS = [
     BASE_DIR / "static"
--- mushahid_blogs/wsgi.py
+++ mushahid_blogs/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mushahid_blogs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mushahid_blogs.settings.production')
 
 application = get_wsgi_application()
